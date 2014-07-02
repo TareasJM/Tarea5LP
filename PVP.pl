642f1fc98b3c2d03a89dc5e88635090e200a9428
n3ggro <n3ggro@gmail.com>
2014-07-01 20:25:11 -0400

    README actualizado. v 1.1

--- PVP.pl
+++ PVP.pl
@@ -7,8 +7,8 @@ aumento(armadura_ligera, [20,40,0]).
 aumento(armadura_completa, [60,0,0]).
 aumento(armadura_normal, [40,20,0]).
 
-%% defenzaTotal calcula la defenza de un personaje + defenza agregada por el arma/armadura
-defenzaTotal(X,DT):- personaje(X,DP,_,_,W,A), aumento(A,[DA,_,_]), aumento(W,[DW,_,_]), plus(DA,DW,Y), plus(Y,DP,DT),!.
+%% defensaTotal calcula la defensa de un personaje + defensa agregada por el arma/armadura
+defensaTotal(X,DT):- personaje(X,DP,_,_,W,A), aumento(A,[DA,_,_]), aumento(W,[DW,_,_]), plus(DA,DW,Y), plus(Y,DP,DT),!.
 
 %% velocidadTotal calcula la velocidad de un personaje + velocidad agregada por el arma/armadura
 velocidadTotal(X,VT):- personaje(X,_,VP,_,W,A), aumento(A,[_,VA,_]), aumento(W,[_,VW,_]), plus(VA,VW,Y), plus(Y,VP,VT),!.
@@ -19,11 +19,11 @@ fuerzaTotal(X,FT):- personaje(X,_,_,FP,W,_), aumento(W,[_,_,FW]), plus(FP,FW,FT)
 %% numeroGolpes calcula en número de golpes que da un personaje usando la formula entregada
 numeroGolpes(X,DE,NG):- fuerzaTotal(X,FT), velocidadTotal(X,VT), U is DE * 100, D is VT * FT, NG is U / D. 
 
-%% pelea compara los NG de cada personaje, si X > Y, retorna X, si no, retorna Y (gracias a la segunda declaración)
-pelea(X,Y,Z):- defenzaTotal(X,DX), defenzaTotal(Y,DY), numeroGolpes(X,DY,GX), numeroGolpes(Y,DX,GY), GX > GY, Z = X.
-pelea(X,Y,Z):- defenzaTotal(X,DX), defenzaTotal(Y,DY), numeroGolpes(X,DY,GX), numeroGolpes(Y,DX,GY), GY > GX, Z = Y,!.
-
+%% pelea compara los NG de cada personaje, si X < Y, retorna X, si no, retorna Y (gracias a la segunda declaración)
+pelea(X,Y,Z):- defensaTotal(X,DX), defensaTotal(Y,DY), numeroGolpes(X,DY,GX), numeroGolpes(Y,DX,GY), GX < GY, Z = X,!.
+pelea(X,Y,Z):- defensaTotal(X,DX), defensaTotal(Y,DY), numeroGolpes(X,DY,GX), numeroGolpes(Y,DX,GY), GY < GX, Z = Y,!.
+pelea(X,Y,Z):- Z = empate,!.
 
 %% declaración de ambos personajes
-personaje(jose, 50, 30, 20, espada_larga, armadura_completa).
-personaje(marco, 40, 40, 20, espada_y_escudo, armadura_normal).
+personaje(theViper, 40, 40, 20, baston, armadura_ligera).
+personaje(theMountain, 30, 10, 60, espada_larga, armadura_completa).
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 Tarea5LP
+
 ========
+
+Alumnos:
+
+	José Miguel Castro Opazo		201273514-9
+	Marco Antonio Salinas Carrasco		201273589-0
+
+========
+
+Personajes creados:
+	theViper: D = 40, V = 40, F = 20, W = baston, A = armadura_ligera.
+	theMountain: D = 30, V = 10, F = 60, W = espada_larga, A = armadura_completa.
+
+La pelea:
+	?- pelea(theViper, theMountain, Winner).
